6f8f562a43b64213afea8fb693d4b4d23a27a287
danil <daniachmetshin@gmail.com>
2024-11-10 18:39:00 +0200

    finished

--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -1,46 +1,11 @@
 import {auth} from '../helper/auth.js';
-import {emptyOrRows} from '../helper/utils.js'
-import {selectAllTasks, insertTask, deleteTask} from '../models/ModelTask.js';
+import {selectAll, addTask, minusTask} from '../controllers/TaskController.js';
 import {Router} from "express";
 
 const todoRouter = Router();
 
-todoRouter.get('/', async (req, res, next) => {
-    try {
-        const result = await selectAllTasks();
-        return res.status(200).json(emptyOrRows(result));
-    } catch (err) {
-    return next(err);
-    }
-});
-
-todoRouter.post('/create', auth, async (req, res, next) => {
-    try {
-        if(!req.body.description || req.body.description.length === 0){
-            const error = new Error("Invalid description for a task")
-            error.statusCode = 400
-            return next(error)
-        }
-        const result = await insertTask(req.body.description);
-        return res.status(200).json(result.rows[0]);
-    } catch (err) {
-    return next(err);
-    }
-});
-
-todoRouter.delete('/delete/:id', auth, async (req, res, next) => {
-    try {
-        if(!parseInt(req.params.id)){
-            const error = new Error('Task id is not provided')
-            error.statusCode = 400
-            return next(error)
-        }
-        const result = await deleteTask(parseInt(req.params.id));
-        // return res.status(200).json(result);
-        return res.status(200).json({id: req.params.id});
-    } catch (err) {
-    return next(err);
-    }
-});
+todoRouter.get('/', selectAll);
+todoRouter.post('/create', auth, addTask);
+todoRouter.delete('/delete/:id', auth, minusTask);
 
 export {todoRouter};
